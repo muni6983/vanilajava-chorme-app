950928fd749036d5ac9d2be88f80ee6bd6233144
muni6983 <75618545+muni6983@users.noreply.github.com>
2020-12-14 15:38:41 +0900

    start

---
+++ index.js
@@ -0,0 +1,19 @@
+/* string
+const what = "Nicolas";
+*/
+/*
+Boolean
+const what = true;
+*/
+/*
+Number
+const what = 666;
+*/
+/* Float
+const what =55.1;
+*/
+
+function sayHello(name, age) {
+    console.log('Hello!', name, "you have", age, "years of age.");
+}
+sayHello("nicolas" , 15);
